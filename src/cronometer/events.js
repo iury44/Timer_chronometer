5e7c8a2fed8e34725ccb2f4a68f3b2854057500a
Iury <iuryhenrique44@gmail.com>
2024-01-22 21:03:48 -0300

    atualização function registerControls e registerSounds trativa erro caso retorne undefinid

--- src/cronometer/events.js
+++ src/cronometer/events.js
@@ -3,14 +3,21 @@ import {controls, cards} from './elements.js'
 
 export function registerControls(){
     controls.addEventListener('click',(event)=>{
-      console.log(event.target.dataset.action)
-    })
-
-    
+     
+      const action = event.target.dataset.action
+      
+      if(action === undefined){ return}
+          
+    console.log(action)
+    })   
 }
 
 export function registerSounds(){
   cards.addEventListener('click', (event)=>{
-    console.log(event.target.dataset.action)
+    const action = event.target.dataset.action
+      
+    if(action === undefined){ return}
+        
+    console.log(action)
   })
 }
